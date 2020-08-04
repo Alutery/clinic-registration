2d61c0ec7aa7b17d7353d0485aaa73dfba9e3e0a
Aliya Safina <ktotamy@gmail.com>
2020-08-04 16:51:08 +0300

    first commit

---
+++ src/components/app/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+
+import './App.css';
+
+function App() {
+  return (
+    <Container className="p-3">
+      <Jumbotron>
+        Clinic Registration
+      </Jumbotron>
+    </Container>
+  );
+}
+
+export default App;
